be214b37cf0ae79b177de04ef16d49d18627513b
htmathl <matheus.domingues126@gmail.com>
2023-03-04 23:34:11 -0300

    subir modulo 4

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Programa
+{
+   static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 6 - Atribuições de variáveis");
+
+        int idade = 30;
+        int idadeAnna = idade;
+
+        Console.WriteLine(idadeAnna);
+
+        idade = 25;
+
+        Console.WriteLine(idadeAnna);
+
+    }
+}
